b6bce658d6cbde7489e28170bd10fd0d6eeeafd1
Finn Terdal <fterdal@gmail.com>
2018-03-09 11:07:10 -0600

    Adds simple Express stuff

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+const homePage = `
+  <!DOCTYPE html>
+  <html>
+    <body>
+      <h1>Home Page</h1>
+      <h2><a href="/people">People</a>
+      <h2><a href="/pets">Pets</a>
+    </body>
+  </html>
+`;
+
+app.get('/', (req, res) => {
+  res.send(homePage);
+})
+
+app.get('/people', (req, res) => {
+  res.send('All people -- Coming Soon');
+})
+
+app.get('/pets', (req, res) => {
+  res.send('All pets -- Coming Soon');
+})
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`server awaiting requests on ${PORT}`));
+
