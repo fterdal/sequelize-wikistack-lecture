e53fa04c84a7e1c98badb2e45543794e351aeae2
Finn Terdal <fterdal@gmail.com>
2018-03-09 15:06:59 -0600

    lecture finished

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
+//const db = require('./db').db;
+//const Person = require('./db').Person;
+const { db, Person, Pet } = require('./db');
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 const homePage = `
   <!DOCTYPE html>
@@ -20,14 +25,36 @@ app.get('/', (req, res) => {
   res.send(homePage);
 })
 
-app.get('/people', (req, res) => {
-  res.send('People -- Coming Soon');
+app.get('/people', (req, res, next) => {
+  Person.findAll()
+    .then(allPeople => {
+      console.log('allPeople', allPeople);
+      res.json(allPeople);
+    })
+    .catch(next)
+})
+ 
+app.post('/people', (req, res, next) => {
+  const { name, imgUrl } = req.body;
+  Person.create({
+    name,
+    imgUrl,
+  })
+  .then(newUser => {
+    console.log(newUser)
+    res.redirect('/people')
+  })
+  .catch(next)
 })
 
 app.get('/pets', (req, res) => {
   res.send('Pets -- Coming Soon');
 })
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`server awaiting requests on ${PORT}`));
+db.sync({ force: true })
+  .then(() => {
+    const PORT = 3000;
+    app.listen(PORT, () => console.log(`server awaiting requests on ${PORT}`)); 
+  })
+  .catch(console.log.bind(console));
 
